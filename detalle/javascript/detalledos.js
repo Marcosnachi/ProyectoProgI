6d2502995bc42cb5abf708290fee9457452f548f
josestaudenmaier <jstaudenmaier@udesa.edu.ar>
2020-06-10 10:25:47 -0300

    todos los detalles semi terminados, faltan cositas

---
+++ detalle/javascript/detalledos.js
@@ -0,0 +1,59 @@
+window.onload = function (){
+    let queryString = window.location.search
+    let objetoQuery= new URLSearchParams(queryString);
+    let elId = objetoQuery.get('id');
+    
+        fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/' + elId)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data)
+            let nombre= document.querySelector('div.infodelartista-4')
+            let foto = document.querySelector('div.profile__img')
+            let categoria = document.querySelector('div.infodelartista-3')
+            let seguidores = document.querySelector("div.infoseguidores")
+            let botones = document.querySelector('div.infodelartista-5')
+            nombre.innerHTML += `<h2> ${data.title} </h2>`
+            foto.innerHTML += '<img src="' + data.cover + '">'
+            categoria.innerHTML += data.type
+            seguidores.innerHTML += data.fans + ' followers'
+            botones.innerHTML += `<a href="../detalle/detalle.html?id=${data.artist.id}"><button class="cosotransparente">Ir al artista<i class="rereproducir"></i></button></a>`
+            
+    
+    
+        })
+        .catch(function(error){
+            console.error(error);
+        });
+        fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/' + elId)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(datos){
+            let canciones = document.querySelector('div.canciones')
+            for (unaCancion of datos.tracks.data) {
+                canciones.innerHTML += `<div class="cancionindividual">
+    
+                <div class="imagenesdecancion"><img src="${datos.cover}" alt="${unaCancion.title}"> </div>
+    
+                <div class="titulodecancion">${unaCancion.title} </div>
+                <div class="titulodealbum">${unaCancion.artist.name} </div>
+                <div class="rereproduccionesdecancion">${unaCancion.duration} s</div>
+    
+            </div>`
+            }
+    
+    
+        })
+        .catch(function(error){
+            console.error(error);
+        });
+    
+    
+    
+            
+        }
+    
+    
+    
---
+++ detalle/javascript/detalletres.js
@@ -0,0 +1,57 @@
+window.onload = function (){
+    let queryString = window.location.search
+    let objetoQuery= new URLSearchParams(queryString);
+    let elId = objetoQuery.get('id');
+    
+        fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/' + elId)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data)
+            let nombre= document.querySelector('div.infodelartista-4')
+            let foto = document.querySelector('div.profile__img')
+            let categoria = document.querySelector('div.infodelartista-3')
+            let seguidores = document.querySelector("div.infoseguidores")
+            let botones = document.querySelector('div.infodelartista-5')
+            nombre.innerHTML += `<h2> ${data.title} </h2>`
+            foto.innerHTML += '<img src="' + data.album.cover + '">'
+            categoria.innerHTML += `${data.type} from: <a href="../detalle/detallealbum.html?id=${data.album.id}"> ${data.album.title}</a>`
+            seguidores.innerHTML += `<a href="../detalle/detalle.html?id=${data.artist.id}"> ${data.artist.name} </a>`
+            botones.innerHTML += `<a href="../detalle/detalle.html?id=${data.artist.id}"><button class="cosotransparente">Ir al artista<i class="rereproducir"></i></button></a>
+            <a href="../detalle/detallealbum.html?id=${data.album.id}"><button class="cosotransparente">Ir al álbum<i class="rereproducir"></i></button></a>`
+    
+        })
+        .catch(function(error){
+            console.error(error);
+        });
+        fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/' + elId)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(datos){
+            let canciones = document.querySelector('div.canciones')
+                canciones.innerHTML += `<div class="cancionindividual">
+    
+                <div class="imagenesdecancion"><img src="${datos.album.cover}" alt="${datos.title}"> </div>
+    
+                <div class="titulodecancion">${datos.title} </div>
+                <div class="titulodealbum">${datos.artist.name} </div>
+                <div class="rereproduccionesdecancion">${datos.duration} s</div>
+    
+            </div>`
+
+    
+    
+        })
+        .catch(function(error){
+            console.error(error);
+        });
+    
+    
+    
+            
+        }
+    
+    
+    
