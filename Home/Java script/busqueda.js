6411e9baa64ff914734f98632a3c5b7c670c679e
marcosnachi <mnachimowicz@udesa.edu.ar>
2020-06-12 12:12:18 -0300

    barra buscadora lista

--- Home/Java script/busqueda.js
+++ Home/Java script/busqueda.js
@@ -1,6 +1,5 @@
 window.onload = function (){
     
-
     //obtengo el query string
     let queryString = window.location.search
 
@@ -8,30 +7,46 @@ window.onload = function (){
     let objetoQuery = new URLSearchParams(queryString);
 
     //ahora si obtengo lo que mandé por el formulario de busqueda
-    let busqueda = objetoQuery.get('filtro');
+    let buscate = objetoQuery.get ('filtro');
     
-    fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=' + busqueda)
-        .then(function(res){
+    //fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=' + busqueda)
+        //.then(function(res){
             //transformo en json
-            return res.json();
+            //return res.json();
             
-        })
+        //})
+    console.log(buscate)
+        
+        fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=' + buscate)
+        .then(function(response){
+            return response.json();
 
-        
-            .then(function(datos){
-                console.log(datos)
-                for (albums of datos.data) {
-                let div = document.getElementById('albums')
-                div.innerHTML += `
-                    <div class="photo-container">
-                       
-                    <a href="../detalle/detallealbum.html?id=${albums.id}"><img src="${albums.cover}" 
-                    alt="${albums.title}"></a>
-                    <div class="text"><h3> ${albums.title}</h3></div>
-                    </div>
-                    `
-        
-        
-                }
-        
-            })}
+        })
+        .then(function(datos){
+        console.log (datos)
+
+            let resultados = document.querySelector('.photo-container')
+            for (let resultado of datos.data)
+                resultados.innerHTML += `
+                <div class="photo-container">
+                   
+                <a href="../detalle/detalle.html?id=${resultado.artist.id}"><img src="${resultado.artist.picture}" 
+                alt="${resultado.artist.name}"></a>
+    
+                <div class="text"><h3> ${resultado.title}</h3></div>
+                </div>
+    
+                `
+
+            
+          
+    
+        })
+        .catch(function(error){
+            console.error(error);
+        });
+    
+    
+    
+            
+        }
